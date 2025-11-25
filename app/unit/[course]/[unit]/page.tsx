7a3cdde86c9b53b07f415ec634b6a4390c9de1ed
jackzacey <jackzacey@gmail.com>
2025-11-25 09:45:55 -0800

    Update page.tsx

--- app/unit/[course]/[unit]/page.tsx
+++ app/unit/[course]/[unit]/page.tsx
@@ -1,7 +1,6 @@
-import ClientInteractive from "./ClientInteractive"; // <<< ADD THIS
+import ClientInteractive from "./ClientInteractive"; // <<< ADD THIS LINE
 
 export async function generateStaticParams() {
-  // compute all { course, unit } combinations
   return [
     { course: "math", unit: "1" },
     { course: "science", unit: "2" },
@@ -13,7 +12,7 @@ export default async function UnitPage({ params }) {
   return (
     <div>
       <h1>Unit {unit} in {course}</h1>
-      <ClientInteractive part={{course,unit}} />
+      <ClientInteractive part={{ course, unit }} />
     </div>
   );
 }
